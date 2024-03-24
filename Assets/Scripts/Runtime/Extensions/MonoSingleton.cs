a5ec741eb1c9f833bb2b3d324aeadb657568933f
atakandll <atakanccnar@outlook.com>
2024-03-24 16:11:05 +0300

    feat(extension): Monosingleton extension added. # Monosingleton adding for signals.

---
+++ Assets/Scripts/Runtime/Extensions/MonoSingleton.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+namespace Runtime.Extensions
+{
+    public class MonoSingleton<T> : MonoBehaviour where T : Component
+    {
+       private static T _instance;
+
+       public static T Instance
+       {
+           get
+           {
+               if (_instance == null)
+               {
+                   _instance = FindObjectOfType<T>();
+                   if (_instance == null)
+                   {
+                       GameObject obj = new GameObject();
+                       _instance = obj.AddComponent<T>();
+                   }
+               }
+               return _instance;
+               
+           }
+       }
+       protected virtual void Awake()
+       {
+           _instance = this as T;
+       }
+           
+       
+    }
+}
